53f37eff28469d0358b6fbe1e7dff22fe8bf1860
BaekSeungHwan <baekgang624@naver.com>
2023-11-07 19:45:03 +0900

    2023 11/07 업로드

---
+++ app/src/main/java/com/example/road_fix_project/mode_choice.java
@@ -0,0 +1,14 @@
+package com.example.road_fix_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mode_choice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
