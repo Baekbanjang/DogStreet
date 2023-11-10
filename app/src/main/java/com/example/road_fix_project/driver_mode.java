5a80b5fd67d7052892f5f3c4267a00a046b32573
BaekSeungHwan <baekgang624@naver.com>
2023-11-10 16:13:33 +0900

    2023 11/10 운전자 모드 지도 기능

--- app/src/main/java/com/example/road_fix_project/driver_mode.java
+++ app/src/main/java/com/example/road_fix_project/driver_mode.java
@@ -1,50 +1,103 @@
 package com.example.road_fix_project;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class driver_mode extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap gMapObj;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    Marker myMarker;
+    Location myLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.driver_mode);
 
-        SupportMapFragment mapFragment;
-        mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this);
+        fetchCurrentLocation();
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        LatLng latLng=new LatLng(37.555744, 126.907431);
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        gMapObj=googleMap;
+        LatLng latLong=new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+        MarkerOptions marker=new MarkerOptions();
+        marker.position(latLong).title("Location");
+        try{
+            marker.snippet(getAddress(myLocation.getLatitude(), myLocation.getLongitude()));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker2));
+        gMapObj.animateCamera(CameraUpdateFactory.newLatLng(latLong));
+        gMapObj.animateCamera(CameraUpdateFactory.newLatLngZoom(latLong,15f));
+        myMarker=gMapObj.addMarker(marker);
+        myMarker.showInfoWindow();
+    }
 
-        MarkerOptions markerOptions=new MarkerOptions();
-        markerOptions.position(latLng);
-        markerOptions.title("서울역");
+    private String getAddress(double latitude, double longitude) throws IOException {
+        Geocoder geocoder=new Geocoder(this, Locale.getDefault());
+        List<Address> addressList=geocoder.getFromLocation(latitude, longitude, 1);
+        return addressList.get(0).getAddressLine(0).toString();
+    }
 
-        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker2));
-        googleMap.addMarker(markerOptions);
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f));
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
-
-        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+    private void fetchCurrentLocation() {
+        if(ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        )
+                != PackageManager.PERMISSION_GRANTED&&
+                ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                !=PackageManager.PERMISSION_GRANTED
+        ){
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    },
+                    1000
+            );
+            return;
+        }
+        Task<Location> task=fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
-            public void onMapClick(@NonNull LatLng latLng) {
-                markerOptions.position(latLng);
-
-                markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker2));
-                googleMap.addMarker(markerOptions);
+            public void onSuccess(Location location) {
+                if(location!=null) {
+                    myLocation=location;
+                    SupportMapFragment mapFragment;
+                    mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    mapFragment.getMapAsync(driver_mode.this);
+                }
             }
         });
     }
