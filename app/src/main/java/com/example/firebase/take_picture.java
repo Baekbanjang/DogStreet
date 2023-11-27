6db5c65462e0296b31c9c011c1afa4ca982bfb91
성준서 <tjdwnstj99@naver.com>
2023-11-27 14:23:58 +0900

    사용자마다 사진 저장

--- app/src/main/java/com/example/firebase/take_picture.java
+++ app/src/main/java/com/example/firebase/take_picture.java
@@ -1,6 +1,8 @@
 package com.example.firebase;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -10,12 +12,21 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+import android.location.Location;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -23,41 +34,61 @@ import java.util.Date;
 
 public class take_picture extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 2;
     private Uri photoURI;
     private StorageReference mStorageRef;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.take_picture);
 
-        Button take_photo = (Button)findViewById(R.id.take_photo);
+        Button take_photo = (Button) findViewById(R.id.take_photo);
         mStorageRef = FirebaseStorage.getInstance().getReference();
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String uid = user.getUid();
+            // Use this uid to store/retrieve user-specific data.
+        }
+
 
         take_photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                    } catch (IOException ex) {
-                        // Error occurred while creating the File
-                    }
-                    if (photoFile != null) {
-                        photoURI = FileProvider.getUriForFile(getApplicationContext(),
-                                "com.example.firebase",
-                                photoFile);
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                    }
+                if (ActivityCompat.checkSelfPermission(take_picture.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(take_picture.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+                    return;
                 }
 
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(take_picture.this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                if (location != null) {
+                                    String locationStr = location.getLatitude() + "_" + location.getLongitude();
+                                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                                        File photoFile = null;
+                                        try {
+                                            photoFile = createImageFile(locationStr);
+                                        } catch (IOException ex) {
+                                            // Error occurred while creating the File
+                                        }
+                                        if (photoFile != null) {
+                                            photoURI = FileProvider.getUriForFile(getApplicationContext(),
+                                                    "com.example.firebase",
+                                                    photoFile);
+                                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                                        }
+                                    }
+                                }
+                            }
+                        });
             }
         });
-
-
     }
 
     @Override
@@ -69,9 +100,9 @@ public class take_picture extends AppCompatActivity {
         }
     }
 
-    private File createImageFile() throws IOException {
+    private File createImageFile(String location) throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
+        String imageFileName = "JPEG_" + timeStamp + "_" + "위치 정보 : " + location + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,
@@ -82,19 +113,23 @@ public class take_picture extends AppCompatActivity {
     }
 
     private void uploadImage() {
-        StorageReference imageRef = mStorageRef.child("images/" + photoURI.getLastPathSegment());
-        imageRef.putFile(photoURI)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        // Task completed successfully
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // Task failed with an exception
-                    }
-                });
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String uid = user.getUid();
+            StorageReference userRef = mStorageRef.child("images/" + uid + "/" + photoURI.getLastPathSegment());
+            userRef.putFile(photoURI)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Task completed successfully
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Task failed with an exception
+                        }
+                    });
+        }
     }
 }
