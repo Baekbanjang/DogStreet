a01dbe00cb453a879b2439b18a6ee8b543ff6e33
성준서 <tjdwnstj99@naver.com>
2023-12-02 20:22:21 +0900

    보행자 모드에서 선택한 신고유형, 사진, 정확한 위치 보여주는 기능과 화면

---
+++ app/src/main/java/com/example/firebase/comprehensive_report.java
@@ -0,0 +1,100 @@
+package com.example.firebase;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.Locale;
+
+public class comprehensive_report extends AppCompatActivity {
+    private ImageView mImageView;
+    private TextView mTextViewAccidentType;
+    private TextView mTextViewLocation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.comprehensive_report);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        String selectedType = sharedPreferences.getString("selectedType", "");
+        String photoPath = sharedPreferences.getString("photoPath", "");
+        String locationStr = sharedPreferences.getString("location", "");
+        mImageView = findViewById(R.id.imageView);
+        mTextViewAccidentType = findViewById(R.id.textView_accident_type);
+        mTextViewLocation = findViewById(R.id.textView_coordinates);
+
+        // 사고 유형을 TextView에 설정
+        mTextViewAccidentType.setText(selectedType);
+        mTextViewLocation.setText("위치: " + locationStr);
+        Button report_Complete= (Button)findViewById(R.id.report_complete);
+        final TextToSpeech[] tts = new TextToSpeech[1];
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String uid = user.getUid();
+            // 이미지를 가져올 경로를 지정
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference().child(photoPath);
+
+            // 최대 이미지 크기를 지정(예: 5MB)
+            long ONE_MEGABYTE = 1024 * 1024 * 5;
+
+            storageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    // Data for "images/uid/your_image_name.jpg" is returned, use this as needed
+                    Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    mImageView.setImageBitmap(bmp);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle any errors
+                }
+            });
+        }
+        report_Complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "신고되었습니다";
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+                // TTS 설정
+                tts[0] = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if (status != TextToSpeech.ERROR) {
+                            tts[0].setLanguage(Locale.KOREAN);
+                            tts[0].speak(message, TextToSpeech.QUEUE_FLUSH, null);
+                        }
+                    }
+                });
+                Intent intent = new Intent(comprehensive_report.this, mode_choice.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
